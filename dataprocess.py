bcc95a0c63f8504b328d31e30e6c3149e54aed15
qshtykoo <35166497+qshtykoo@users.noreply.github.com>
2018-11-18 17:09:42 +0200

    add one more method in collect_data class

--- dataprocess.py
+++ dataprocess.py
@@ -33,6 +33,19 @@ class collect_data:
 
         return rwave_list
 
+    def readFilteredWave(self):
+        fwave_list = []
+        csv_files = os.listdir(self.file_dir)
+        targeted_str = "filtered"
+        for csv_file in csv_files:
+            if targeted_str in csv_file:
+                csv_full_path = os.path.join(self.file_dir, csv_file)
+                filtered_wave = pd.read_csv(csv_full_path)
+                fwave_list.append(filtered_wave)
+
+        return fwave_list
+
+
 class process_data:
 
     def __init__(self, data_list):
@@ -66,4 +79,27 @@ if __name__=="__main__":
     #first_second = raw_psec.loc[0:7, :]
     selected_time = np.sum(raw_psec.iloc[0:31, 1:122], axis =0)
     plt.plot(header_list_filtered, selected_time)
+    plt.show()
+
+    rwave_list = CD.readRawWave()
+    rwave = rwave_list[0]
+
+    fwave_list = CD.readFilteredWave()
+    fwave = fwave_list[0]
+
+
+    fig, axs = plt.subplots(2,1, constrained_layout=True)
+    axs[0].plot(np.array(range(len(rwave))), rwave[" Value"])
+    axs[0].set_title('Raw Wave Data')
+    axs[0].set_xlabel('Time')
+    axs[0].set_ylabel('Voltage')
+    axs[0].set_ylim([-200, 200])
+    fig.suptitle('Raw Wave Data in Time Domain')
+
+    axs[1].plot(np.array(range(len(fwave))), fwave[" Value"])
+    axs[1].set_title('Raw Filtered Wave Data')
+    axs[1].set_xlabel('Time')
+    axs[1].set_ylabel('Voltage')
+    axs[1].set_ylim([-200, 200])
+
     plt.show()
