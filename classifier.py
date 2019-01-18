d18d935bd5470d9931efc8f7b3c7f08dd0e558b5
qshtykoo <35166497+qshtykoo@users.noreply.github.com>
2019-01-18 18:10:22 +0200

    implemented adaboost and naive Bayes

--- classifier.py
+++ classifier.py
@@ -8,19 +8,36 @@ import pandas as pd
 import numpy as np
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.metrics import confusion_matrix
-import csv
+from sklearn.naive_bayes import GaussianNB
 
-beach_data = pd.read_csv("beach.csv", header=None)
+def error_rate(y_true, y):
+    err = 0
+    for i in range(y_true.shape[0]):
+        if y[i] != y_true.iloc[i,0]:
+            err = err + 1
+    return err / y.shape[0]
 
-train_x = beach_data.iloc[:, :402]
-train_y = beach_data.iloc[:, 402:]
-ada = AdaBoostClassifier(n_estimators=50)
+beach_data = pd.read_csv("finger.csv", header=None)
+
+X = beach_data.iloc[:, :402]
+y = beach_data.iloc[:, 402:]
+
+test_index = list(range(4,135,5))
+train_index = list(X.index)
+for i in test_index:
+    train_index.remove(i)
+
+test_x = X.iloc[test_index, :]
+test_y = y.iloc[test_index, :]
+train_x = X.iloc[train_index, :]
+train_y = y.iloc[train_index, :]
+    
+gnb = GaussianNB()
+y_gnb = gnb.fit(train_x, train_y).predict(test_x)
+#graph = confusion_matrix(test_y, y_gnb)
+
+ada = AdaBoostClassifier(n_estimators=7)
 ada.fit(train_x, train_y)
-y = ada.predict(train_x)
-graph = confusion_matrix(train_y, y)
+y_ada = ada.predict(train_x)
 
-err = 0
-for i in range(train_y.shape[0]):
-    if y[i] != train_y.iloc[i,0]:
-        err = err + 1
-err_rate = err / train_y.shape[0]
+err_rate = error_rate(train_y, y_ada)
